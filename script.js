4d54f263ac263fbd3a8382daa249a04c9c8ee961
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-06-27 12:52:19 +0530

    Assignment Operators

--- script.js
+++ script.js
@@ -1,64 +1,34 @@
 // .......................................
-// .....Arithmetic Operators..............
+// .....Assignment Operators..............
 // .......................................
 
-// -- Single Operators --
+// Equal
+var myName = "Dhishan";
+console.log(myName);
 
-var myAge, yourAge, year, result;
-myAge = 29;
-yourAge = 20;
-year = 2023;
+// Addition wuth Equal
+var age = 29;
+age += 10; // age = age + 10;
+console.log(age);
 
-// Substraction
-result = year - myAge;
-console.log("I was born in " + result);
+// Substrcation with Equal
+age -= 10; // age = age - 10;
+console.log(age);
 
-// Addition
-result = myAge + 10;
-console.log("After 10 yers my age " + result);
+// Multiplication with Equal
+age *= 10; // age = age * 10;
+console.log(age);
 
-// Multiplication
-result = myAge * yourAge;
-console.log("Multiplication your age and my age " + result);
+// Division with Equal
+age /= 10; // age = age / 10;
+console.log(age);
 
-// Division
-console.log("Division of our ages " + (myAge / yourAge));
+// Mudulus
+age %= 10; // age = age % 10;
+console.log(age);
 
-// Modulus
-console.log("Modulus of our ages " + (myAge % yourAge));
-
-// Exponentiation
-console.log("Exponent of my age " + (myAge**2));
-console.log("Exponent of my age " + (myAge**3));
-
-
-//--- Increment--- 
-// Post Increment
-console.log(myAge++);
-console.log("Post Increment my age " + myAge);
-
-// Pre Increment
-console.log(++myAge);
-console.log("Pre Increment my age " + myAge);
-
-//--- Decrement--- 
-// Post Decrement
-console.log(myAge--);
-console.log("Post Decrement my age " + myAge);
-
-// Pre Decrement
-console.log(--myAge);
-console.log("Pret Decrement my age " + myAge);
-
-//-- Operator Precedence -- 
-var cal = (((1/4)+(1/4))*4)**3;
-console.log(cal);
-
-// Multiple Operators
-var ans1, ans2;
-ans1 = ans2 = (((1/4)+(1/4))*4)**3 + 5;
-console.log(ans1);
-
-
+// Exponentiation with Equal
+age **= 2; // age = age ** 2;
+console.log(age);
 
 
