be8ee68243e6f3ee06a86aa7ad8ba66c53112d31
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-06-27 15:35:39 +0530

    Comparison Operators

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 // .......................................
-// ...............If Else..............
+// .........Comparison Operators...........
 // .......................................
 
-var a = 30;
-var b = 40;
+var a = 20;
+var b = "20";
+var c = 5;
+var d = 15;
 
-if(a > b){
-    console.log("a is greater then b")
+// Equal to
+if(a===b){ // ==
+    // Equal to value and Equal type
+    if(a===b){
+        console.log("Borth value and type of a equal to b");
+    }
+    else if(a !== b){
+        console.log("Only value or type of a equal to b");
+    }
 }
-else if(a < b){
-    console.log("a is less then b")
+
+// Greater than or equal
+else if(a == 10){   // >=
+    if(a > 10){
+    // Greater than 
+    console.log("a is greater than 10");
+    }
+    else if(a == 10){
+    // Equal top
+    console.log("a is equal to 10");
+    }
 }
-else (a == b){
-    console.log("a is equal b")
+
+// Not Equal
+else if(a == c){ // !=
+    // less then
+    if(a < c){
+        console.log("a is less than c");
+    }
+    // Greater then
+    if(a > c){
+        console.log("a is Greater than c");
+    }
 }
+
+else{
+    // Less than or equal to
+    if(c <= d){
+        // if(c <= 5){
+        //     console.log("c is Less than equal to 5");
+        // }
+        // else{
+        //     console.log("c is greater than 5");
+        // }
+
+        // Ternary Operater
+        c <= 5 ? console.log("c is Less than equal to 5") : console.log("c is greater than 5");
+    }
+
+}
+
