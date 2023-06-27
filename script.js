7c24f988fd9715b85722821c132667b790c2bbe8
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-06-27 17:56:12 +0530

    Logical Operator

--- script.js
+++ script.js
@@ -1,60 +1,27 @@
 // .......................................
-// .........Comparison Operators...........
+// ..........Logical Operators............
 // .......................................
 
 var a = 20;
-var b = "20";
+var b = 10;
 var c = 5;
-var d = 15;
 
-// Equal to
-if(a===b){ // ==
-    // Equal to value and Equal type
-    if(a===b){
-        console.log("Borth value and type of a equal to b");
-    }
-    else if(a !== b){
-        console.log("Only value or type of a equal to b");
-    }
+// And
+if(a == 10 && a == b){
+    console.log("It is good!");
 }
 
-// Greater than or equal
-else if(a == 10){   // >=
-    if(a > 10){
-    // Greater than 
-    console.log("a is greater than 10");
-    }
-    else if(a == 10){
-    // Equal top
-    console.log("a is equal to 10");
-    }
+// or
+else if (a < 10 || c > b){
+    console.log("It is ok!");
 }
 
-// Not Equal
-else if(a == c){ // !=
-    // less then
-    if(a < c){
-        console.log("a is less than c");
-    }
-    // Greater then
-    if(a > c){
-        console.log("a is Greater than c");
-    }
-}
+// not
+else if(a != c){
+    console.log("It is normal!");
+} 
 
 else{
-    // Less than or equal to
-    if(c <= d){
-        // if(c <= 5){
-        //     console.log("c is Less than equal to 5");
-        // }
-        // else{
-        //     console.log("c is greater than 5");
-        // }
-
-        // Ternary Operater
-        c <= 5 ? console.log("c is Less than equal to 5") : console.log("c is greater than 5");
-    }
-
+    console.log("It is bad!");
 }
 
